3a0b738f8b5ee1edd41720c9e85b2cdb42e18343
Michael Barlock <barlock@users.noreply.github.com>
2020-02-20 11:27:37 -0500

    🔧 Update build process to use references (#44)
    * 🔧 Update build process to use references
    * remove unnecessary tsconfig rule
    * -p -> -b for koa-bolt
    
    Co-authored-by: Nick Bartlett <nbartlett7@gmail.com>

--- packages/fixtures/src/index.ts
+++ packages/fixtures/src/index.ts
@@ -2,6 +2,4 @@ import * as events from './events';
 import fields from './fields';
 import ServerlessTester from './serverless-tester';
 
-// Plans to add more fixtures like `web` for responses
-// eslint-disable-next-line import/prefer-default-export
 export { events, fields, ServerlessTester };
